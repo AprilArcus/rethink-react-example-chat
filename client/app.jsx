9d0bdbc63c414900bae9782e1782cfd0cb703448
April Arcus <april.arcus@gmail.com>
2015-10-12 16:39:09 -0700

    fork from https://github.com/mikemintz/react-rethinkdb/tree/master/examples/chat/server

---
+++ client/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import {AuthWrapper} from './AuthWrapper.jsx';
+
+const mountNode = document.getElementById('app');
+React.render(<AuthWrapper />, mountNode);
---
+++ server/resetdb.js
@@ -0,0 +1,32 @@
+import Promise from 'bluebird';
+import r from 'rethinkdb';
+import cfg from './config';
+
+const connPromise = Promise.promisify(r.connect)({
+  host: cfg.dbHost,
+  port: cfg.dbPort,
+  db: cfg.dbName,
+});
+const run = q => connPromise.then(c => q.run(c));
+
+console.log('Resetting chat db...');
+
+const recreateDb = name => run(r.dbDrop(name))
+                           .catch(() => {})
+                           .then(() => run(r.dbCreate(name)));
+
+const recreateTable = name => run(r.tableDrop(name))
+                              .catch(() => {})
+                              .then(() => run(r.tableCreate(name)));
+
+recreateDb(cfg.dbName).then(() => (
+  Promise.all([
+    recreateTable('messages').then(() => (
+      run(r.table('messages').indexCreate('createdAt'))
+    )),
+    recreateTable('users'),
+  ])
+)).then(() => {
+  connPromise.then(c => c.close());
+  console.log('Completed');
+});
