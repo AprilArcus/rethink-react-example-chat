7c43fcbf3473209f0d586767305c4eab7a392db3
April Arcus <april.arcus@gmail.com>
2015-10-28 21:30:04 -0700

    es6ify devServer

--- client/devServer.js
+++ client/devServer.js
@@ -1,23 +1,26 @@
 import path from 'path';
 import express from 'express';
 import webpack from 'webpack';
-import config from './webpack.config.dev';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from './webpack.config';
+
 
 const app = express();
 const compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
+app.use(webpackDevMiddleware(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }));
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(webpackHotMiddleware(compiler));
 
-app.get('*', function(req, res) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(3000, 'localhost', (err) => {
   if (err) {
     console.log(err);
     return;
