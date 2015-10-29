0fd2717d880ae54e11262ef20d5bf2dc34969dfa
April Arcus <april.arcus@gmail.com>
2015-10-28 20:26:01 -0700

    chatMessage -> pure function

--- client/src/ChatMessage.js
+++ client/src/ChatMessage.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import pureRenderMixin from 'react-addons-pure-render-mixin';
 import moment from 'moment';
 
-export const ChatMessage = React.createClass({
-  mixins: [pureRenderMixin],
+export function ChatMessage({curUserId, userId, body, createdAt}) {
 
-  render() {
-    const {curUserId, userId, body, createdAt} = this.props;
-    return (
-      <div className={'message ' + (userId === curUserId ? 'to' : 'from')}>
-        <div className="messageSender">
-          {userId}
-        </div>
-        <div className="messageBody">
-          {body}
-          <div className="timestamp">
-            {moment(createdAt).calendar()}
-          </div>
+  return (
+    <div className={'message ' + (userId === curUserId ? 'to' : 'from')}>
+      <div className="messageSender">
+        {userId}
+      </div>
+      <div className="messageBody">
+        {body}
+        <div className="timestamp">
+          {moment(createdAt).calendar()}
         </div>
       </div>
-    );
-  },
-});
+    </div>
+  );
+
+}
